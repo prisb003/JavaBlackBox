5ff5c27533f4d4a2c726a36b5b1b4ca24dac115e
Sam <prisb003@mymail.unisa.edu.au>
2026-07-02 14:42:55 +0930

    Added Event Interface
    It stores 4 attributes for any given event. ID, Timestamp, Name and Category

---
+++ src/com/javablackbox/events/Event.java
@@ -0,0 +1,16 @@
+// Use events package and import libraries for globally unique IDs and storing time stamps as objects not strings
+package com.javablackbox.events;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public interface Event {
+	// Every event has an ID, Timestamp, Name and Category
+	UUID getId();
+	
+	Instant GetTimeStamp();
+	
+	String getName();
+	
+	EventCategory getCategory();
+}
